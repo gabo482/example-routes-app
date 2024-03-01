fc6c58379f0c10f02f35e8b08b06f292562273e0
gabo482 <vcruztesci@gmail.com>
2024-03-01 13:03:40 -0600

    Se agrega ejemplo de uso de rutas con angular 17

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes , RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+];
+ 
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
